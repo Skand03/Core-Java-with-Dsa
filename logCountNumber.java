99e210321f9d652b4b907c6a4789852178d36bf9
Skand Chaubey <112958864+Skand03@users.noreply.github.com>
2025-07-16 21:04:08 +0530

    Add files via upload

---
+++ logCountNumber.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class logCountNumber {
+    public static void main(String[] args) {
+        int n = 563214595;
+        int count = (int)(Math.log10(n)+1);
+        System.out.println(count);
+
+    }
+}
